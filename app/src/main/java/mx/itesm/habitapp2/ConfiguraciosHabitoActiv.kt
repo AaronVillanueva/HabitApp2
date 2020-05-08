6f4e816a453e71065c72e5f694d6fdb26e4260b3
Pachada <dtrejog98@gmail.com>
2020-05-08 13:54:56 -0500

    Se agreago la segunda pantalla al darle click a los habitos

---
+++ app/src/main/java/mx/itesm/habitapp2/ConfiguraciosHabitoActiv.kt
@@ -0,0 +1,12 @@
+package mx.itesm.habitapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfiguraciosHabitoActiv : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracios_habito)
+    }
+}
