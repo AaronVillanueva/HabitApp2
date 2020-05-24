147010db429ce0da3c08f9895e9fcc71a08caf89
CINCONUMBERCERO <cinconumbercero@hotmail.com>
2020-05-23 23:16:39 -0500

    iniciado proceso para notificaciones programadas

---
+++ app/src/main/java/mx/itesm/habitapp2/NotificationJob.kt
@@ -0,0 +1,7 @@
+package mx.itesm.habitapp2
+//https://developer.android.com/training/run-background-service/send-request
+//https://developer.android.com/training/run-background-service/create-service
+//https://developer.android.com/guide/components/broadcasts
+class NotificationJob {
+    
+}
